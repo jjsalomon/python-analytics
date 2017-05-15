052cc025b742ed3b6bd6ed8fd4d4b2dbd12fd4e3
azuraiii <c14429822@mydit.ie>
2017-05-15 02:02:04 +0100

    Added numpy9 Woo!

---
+++ numpy/numpy9 - Reading and Saving Data in Binary Files.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 15 01:36:29 2017
+
+@author: azkei
+Reading and Writing Array Data to Files
+"""
+# 1. Loading and Saving Data in Binary Files
+# Regarding saving and the nlater retrieving the data stored in binary format
+# NumPy provides a pair of functions called save() and load()
+
+# Generate Data
+data = np.random.random((4,3))
+
+# Save Data
+np.save('saved_data',data)
+
+# When you need to recover the data stored in the .npy file
+loaded_data = np.load('saved_data.npy')
+loaded_data
+
+# 2. Reading File with Tabular Data
+data = np.genfromtxt('data.csv',delimiter = ',',names = True)
+
+# As a result, we get a structure array which the column headings have 
+# become the names of the field
+
+# The function performs two loops:
+#   1. Reads a line at a time
+#   2. Seperates and converts the values contained in it,
+#       inserting the consecutive elements created specifically.
+
+# One positive aspect of this feature is that if the file contains missing data
+# the function knows how to handle it
+data2 = np.genfromtxt('data2.csv',delimiter=',',names = True)
