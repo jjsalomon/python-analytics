1dcef4c9f661d9fdcdef60b2ed399cf0919b18cf
azuraiii <c14429822@mydit.ie>
2017-05-25 21:25:34 +0100

    pandas1 v2 done

--- pandas1 - Introduction to Pandas/pandas10 - Heirarchical Indexing & Leveling.py
+++ pandas1 - Introduction to Pandas/pandas10 - Heirarchical Indexing & Leveling.py
@@ -19,3 +19,53 @@ mser.index
 mser['white']
 # Or you can select values for a given value of the second index.
 mser[:,'up']
+
+# If you want to specify a single value, you specify both indexes
+mser['white','up']
+
+# The heirarchical index plays a key role in reshaping data and group based operations
+# such as pivot-tables, the data can be converted into a dataframe format using
+# unstack() - this function converts Series with heirarchical data into a
+# simple DataFrame.
+mser.unstack()
+
+# To do reverse operation i.e. convert a DataFrame into a Series.
+# Use a stack() function
+frame.stack()
+
+# As regards the DataFrame, it is possible to define a hierarchical index both
+# for the rows and for the columns.
+# At the time of the declaration of the DataFrame, #
+# you have to define an array of arrays for both the index option and column option
+mframe = pd.DataFrame(np.random.rand(16).reshape(4,4),
+                      index=[['white','white','red','red'],
+                             ['up','down','up','down']],
+                             columns=[['pen','pen','paper','paper'],
+                                      [1,2,1,2]])
+mframe
+
+# 2. Reordering & Sorting Levels
+# Occasionally, you could need to rearrange the order of the levels on an axis
+# or do sorting for values at a specific level.
+# The swaplevel() accepts as a argument the names assigned to the two levels
+# you want to interchange, and returns a new object with the two levels
+# interchanged between them. Leaving the data unmodified.
+
+# Column Labels
+mframe.columns.names=['objects','id']
+# Row Labels
+mframe.index.names=['colors','status']
+mframe
+
+# Interchange rows
+mframe.swaplevel('colors','status')
+
+# sortlevel() - orders the data considering only those of a certain level
+mframe.sortlevel('colors')
+
+# 3. Summary Statistic by Level
+# Make a Summary Statistic at Row level.
+mframe.sum(level='colors')
+# Make a Summary statistic for a given level of column, e.g. id
+# You will need to specify the axis which it lies on
+mframe.sum(level='id',axis=1)
---
+++ pandas2 - Reading & Writing Data/pandas1 - Reading Data in CSV or Text Files.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 25 16:17:03 2017
+
+@author: azkei
+Reading Data in CSV or Text Files
+"""
+#
+
+
