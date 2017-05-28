0279b7bf4f0ff1fd5dad358f6ca655f76820391f
azuraiii <c14429822@mydit.ie>
2017-05-28 17:00:47 +0100

    pandas3 done

--- pandas2 - Reading & Writing Data/pandas2 - Reading & Writing HTML Files.py
+++ pandas2 - Reading & Writing Data/pandas2 - Reading & Writing HTML Files.py
@@ -3,6 +3,43 @@
 Created on Fri May 26 17:52:10 2017
 
 @author: azkei
-"""
+Pandas provides the corresponding pair of I/O API functions for HTML.
 
-#
+read_html()
+to_html()
+
+"""
+# 1. Writing Data in HTML
+# Generate Data
+frame = pd.DataFrame(np.arange(4).reshape(2,2))
+# Using to_html()
+print(frame.to_html())
+# The Data Structure was formed to HTML table according to the DataFrame
+
+frame = pd.DataFrame(np.random.random((4,4)),
+                     index=['white','black','red','blue'],
+                     columns=['up','down','right','left'])
+frame
+# Writing a html page through generation of a string.
+# First we create a string that contains the code of the HTML page.
+s=['<HTML>']
+s.append('<HEAD><TITLE>My DataFrame</TITLE></HEAD>')
+s.append('<BODY>')
+s.append(frame.to_html())
+s.append('</BODY></HTML>')
+html=''.join(s)
+# Once all the listings contained within the HTML varliabl,
+# You can write directly to the file
+html_file = open('myFrame.html','w')
+html_file.write(html)
+html_file.close()
+
+# 2. Reading Data from an HTML File.
+# The read_html() function returns a list of DataFrame even if there is only
+# one table.
+# If there is more than one table in the html, just change the index.
+web_frames = pd.read_html('myFrame.html')
+web_frames[0]
+# Reading a table from a URL link.
+ranking = pd.read_html('http://www.meccanismocomplesso.org/en/meccanissmo-complesso-sit-2/classifica-punteggio/')
+ranking[0]
---
+++ pandas2 - Reading & Writing Data/pandas3 - Reading Data from XML.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 28 14:51:28 2017
+
+@author: azkei
+"""
+
+#
